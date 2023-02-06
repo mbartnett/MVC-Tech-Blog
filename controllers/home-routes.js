98d2c31525f4a3347683c0d1057a5c27c6f62a90
Mike B <115735557+mbartnett@users.noreply.github.com>
2023-02-05 18:05:16 -0600

    updating post tutoring

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,18 +1,51 @@
 const express = require('express');
 const router = express.Router();
 const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
     Post.findAll({
         include: [
             {
-                model: User,
-                attributes: ['username']
+                model: User
             }
         ]
     }).then((posts) => {
         console.log(posts);
+        posts = posts.map((post) => post.get({ plain: true }));
         res.render('home', { posts });
+    });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/signup/', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('signup');
+});
+
+router.get('/dashboard', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        include: [
+            {
+                model: User
+            }
+        ]
+    }).then((posts) => {
+        posts = posts.map((post) => post.get({ plain: true }));
+        res.render('dashboard', { posts });
     });
 });
 
