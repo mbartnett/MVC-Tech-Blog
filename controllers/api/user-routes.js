bf8170ec69cb03461465df5c85f2f972688db61e
Mike B <115735557+mbartnett@users.noreply.github.com>
2023-02-07 17:35:22 -0600

    more work on routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -43,10 +43,6 @@ router.post('/login', async (req, res) => {
 
             res.json({ user: userData, message: 'You are now logged in!' });
         });
-
-    // } catch (err) {
-    //     res.status(400).json(err);
-    // }
 });
 
 router.post('/logout', (req, res) => {
